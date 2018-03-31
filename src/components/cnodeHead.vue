d140240dc6c8c180f8ea6cd7c9eda1fee01dcd62
hoo null <null@hoodeMacBook-Pro.local>
2018-03-31 15:37:11 +0800

    give the commponent cnode-head a piece of css style

---
+++ src/components/cnodeHead.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class='head'>
+            <div class='dialogDiv'>
+                <span>
+                    <strong>作者: &nbsp;&nbsp;</strong>
+                </span>
+                <a href='http://nulls.cc/' target='_blank'>null</a>
+                </span>
+                <br>
+                <span>
+                    <strong>源代码: &nbsp;&nbsp;</strong>
+                </span>
+                <a href='https://github.com/nullscc/cnode' target='_blank'>Github</a>
+                <span class='star'>&nbsp;&nbsp;Star Star</span>
+                <br>
+                <br>
+                <span>
+                    <strong>技术栈: </strong>
+                </span>
+                <ul>
+                    <li>Vue.js</li>
+                    <li>Vue-router</li>
+                    <li>Axios</li>
+                    <li>Element</li>
+                    <li>CnodeJS API</li>
+                </ul>
+            </div>
+    </div>
+</template>
+
+<style>
+.dialogDiv {
+  outline: solid 2px cyan;
+  margin: 0 10em;
+}
+</style>
